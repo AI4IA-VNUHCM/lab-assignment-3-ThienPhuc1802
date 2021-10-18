a2534b105b970ea241fba6a19e912076b26693f8
ThienPhuc1802 <91868450+ThienPhuc1802@users.noreply.github.com>
2021-10-18 16:34:22 +0700

    ex3

--- src/Exercise3.c
+++ src/Exercise3.c
@@ -13,8 +13,42 @@ Ex:
 #include <math.h>
 
 void Ex3(int in_arr[], int n){
-	//Your codes here
+	int i,j,d,c,e,f,k,h;
+	c=0;
+	d=0;
+	e=0;
+	f=0;
+	for(i=0,j=1;i<n-1 && j<n;i++,j++){
 	
+	if(in_arr[i]<in_arr[j]){
+		c+=1;
+		d=0;
+		if (c>e){
+		e=c;
+		k=i;}
+
+	}
+	if(in_arr[i]>in_arr[j]){
+		 d+=1;
+		 c=0;
+		 if (d>f){
+		 f=d;
+		 h=i;
+		 }
+	}
+	
+}   
+    e+=1;
+	f+=1;
+   
+	printf("Increasing");
+	for (i=k+2-e;i<=k+1;++i){
+		printf(" %d",in_arr[i]);
+	}
+	printf(" Decreasing");
+	for (i=h+2-f;i<=h+1;++i){
+		printf(" %d", in_arr[i]);
+	}
 }
 
 int main(int argc, char *argv[]) {
