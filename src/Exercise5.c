044dae7e0848833791e5bc79899217aaff909bd5
ThienPhuc1802 <91868450+ThienPhuc1802@users.noreply.github.com>
2021-10-18 15:47:37 +0700

    ex5

--- src/Exercise5.c
+++ src/Exercise5.c
@@ -12,7 +12,20 @@ ______________________________________
 #include <math.h>
 
 void Ex5(int arr[], int n){
-	//Your codes here
+	int i,j;
+	for (int i = 0; i <n ; i++) {
+        for (int j = i + 1; j < n; j++) {
+			if (arr[i]<0 && arr[j]>0 ){
+             
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+	
+            }
+		}	 
+    }
+	for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);}
 	
 }
 
